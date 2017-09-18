db6ec848ae3ddc0689e92b6fa2126e8f3cacc8f1
Michael Kemp <mkemp@accu-time.com>
2017-09-18 10:13:49 -0400

    Initial commit

---
+++ desktop/src/com/mygdx/camerabasics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.camerabasics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.camerabasics.CameraBasics;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CameraBasics(), config);
+	}
+}
